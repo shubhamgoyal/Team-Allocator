231a8c861f2f59cf15056e1ae43389eb2672d76d
Sahil Bajaj <sahil29@gmail.com>
2014-11-17 20:29:15 +0800

    save events to db and display them

--- team-allocator/team-allocator.js
+++ team-allocator/team-allocator.js
@@ -13,7 +13,21 @@ if (Meteor.isClient) {
   Template.hello.helpers({
     counter: function () {
       return Session.get("counter");
-    }
+    },
+
+	  events: function () {
+		  return Events.find({});
+	  }
+  });
+
+  Template.event.events({
+	  'click #create-event': function (event) {
+		  Events.insert({
+			  'name': $('#event-name').val()
+		  });
+
+		  return false;
+	  }
   });
 
   Template.hello.events({
